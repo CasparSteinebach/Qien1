74576b5093026c666a86ab6bce9331dbb31c2344
CasparSteinebach <casparsteinebach@gmail.com>
2020-06-08 19:49:24 +0200

    eerste foto

---
+++ Mastermind.java
@@ -0,0 +1,13 @@
+package com.company;
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.*;  // Import the Scanner class
+public class Mastermind {
+
+
+    private static int nieuweStand;
+
+    public static void main(String[] args) {
+
+    }
+    }
